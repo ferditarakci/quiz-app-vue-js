acc091570f3debd487adca4267adfa9f649d65af
Ferdi Tarakcı <bilgi@ferditarakci.com.tr>
2025-07-30 20:37:12 +0300

    Upgraded from Vue CLI to Vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
 import('./loadEnv')
@@ -24,7 +25,10 @@ export default async ({ mode }) => {
     plugins: [vue()],
     resolve: {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-      //...
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        src: fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
   })
 }
