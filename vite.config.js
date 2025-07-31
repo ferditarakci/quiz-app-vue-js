c84589101adb7a1585a4004cfcc78f54f310e229
Ferdi Tarakcı <bilgi@ferditarakci.com.tr>
2025-07-31 21:16:49 +0300

    Upgraded from Vue CLI to Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
 import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import mkCert from 'vite-plugin-mkcert'
 
 import('./loadEnv')
 
@@ -14,6 +16,8 @@ export default async ({ mode }) => {
     DEBUG: process.env.DEBUG,
   }
 
+  const DEBUG = process.env.DEBUG
+
   return defineConfig({
     define: {
       'process.env': env,
@@ -22,7 +26,37 @@ export default async ({ mode }) => {
       // https://stackoverflow.com/questions/77288512/vite-vue-3-built-project-content-security-policy-error-csp-script-src-self
       __INTLIFY_JIT_COMPILATION__: true,
     },
-    plugins: [vue()],
+    plugins: [vue(), mkCert()],
+    root: './src',
+    publicDir: '../public',
+    esbuild: {
+      keepNames: false,
+      minify: !DEBUG,
+      minifyIdentifiers: false,
+      minifyWhitespace: !DEBUG,
+      minifySyntax: !DEBUG,
+      legalComments: 'none',
+    },
+    build: {
+      minify: 'esbuild',
+      outDir: '../dist',
+      emptyOutDir: true,
+      copyPublicDir: true,
+      rollupOptions: {
+        // external: [
+        //   'src/scripts/client.js'
+        // ],
+        input: {
+          app: 'src/index.html',
+        },
+
+        output: {
+          assetFileNames: 'assets/[hash].[ext]', // 'assets/[name]-[hash].[ext]'
+          chunkFileNames: 'scripts/[hash].js', // 'scripts/[name]-[hash].js'
+        },
+      },
+    },
+    server: mode !== 'production' ? { https: true } : {},
     resolve: {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
       alias: {
